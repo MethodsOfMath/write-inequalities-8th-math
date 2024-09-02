ba2f902e8471a10d6ecbe973ece96a33afd4c363
Scott Geyer <scott.fred.geyer@gmail.com>
2024-09-02 14:16:40 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -107,5 +107,12 @@ function nextQuestion() {
   }
 }
 
+function previousQuestion() {
+  if (currentQuestion > 0 ) {
+    currentQuestion--;
+    showQuestion(currentQuestion);
+  }
+}
+
 showQuestion(0);
 
